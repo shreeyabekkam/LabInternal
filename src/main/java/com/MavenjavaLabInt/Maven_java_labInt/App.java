96ab5f264e2c42fd735b530ff4cb8f0139a6b7e6
shreeyabekkam <shreeyabekkam@gmail.com>
2022-11-17 13:10:16 +0530

    first commit

---
+++ src/main/java/com/MavenjavaLabInt/Maven_java_labInt/App.java
@@ -0,0 +1,13 @@
+package com.MavenjavaLabInt.Maven_java_labInt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World LabInternal 20bd1a66b2" );
+    }
+}
